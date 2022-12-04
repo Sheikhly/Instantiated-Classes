e4c7a1cc0774cb7117c974ea18e1aca5e8111c83
HP <HP@DESKTOP-7T8T92K>
2022-12-04 10:52:39 +0330

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleUI;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    /*
+     * In this App We Want to Take a quick look of How Instantiation Works
+     */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserMassages.SayHello();
+
+            PersonModel person = new PersonModel();
+
+            person.firstName = "Sina";
+
+            PersonModel secondperson = new PersonModel();
+
+            secondperson.firstName = "Elahe";
+
+            Console.WriteLine(person.firstName);
+
+            Console.WriteLine(secondperson.firstName);
+
+
+            Console.ReadLine();
+        }
+    }
+}
